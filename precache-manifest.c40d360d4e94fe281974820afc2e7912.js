d69efb5b32f2db87eece7f5c0dc4418c1ef48c02
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-29 19:40:27 +0700

    Add files via upload

---
+++ precache-manifest.c40d360d4e94fe281974820afc2e7912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089ae5afe5934225ce7d3115f196d1f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc20ffa7e594d9e5b8d",
+    "url": "/static/css/main.9e213fea.chunk.css"
+  },
+  {
+    "revision": "fe0f858930b2ba70b959",
+    "url": "/static/js/2.c20edc23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c20edc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc20ffa7e594d9e5b8d",
+    "url": "/static/js/main.e4ddf8c2.chunk.js"
+  },
+  {
+    "revision": "d7a6e5881c5d479b7594",
+    "url": "/static/js/runtime-main.2ea4cdbd.js"
+  }
+]);
